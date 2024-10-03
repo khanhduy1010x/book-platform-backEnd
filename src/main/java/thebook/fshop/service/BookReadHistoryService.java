ab6bc1c9b75f33b31bb6d9a3ea2db7b02e785cd8
Haolambor <83999494+Haolambor@users.noreply.github.com>
2024-10-03 14:19:57 +0700

    BookReadHistory Done! first commit

--- src/main/java/thebook/fshop/service/BookReadHistoryService.java
+++ src/main/java/thebook/fshop/service/BookReadHistoryService.java
@@ -4,11 +4,44 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import thebook.fshop.DTO.Response.BookReadHistoryResponse;
+import thebook.fshop.entity.BookReadHistory;
+import thebook.fshop.entity.Account;
+import thebook.fshop.entity.Book;
+import thebook.fshop.mapper.BookReadHistoryMapper;
+import thebook.fshop.repository.BooKReadHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
 public class BookReadHistoryService {
+BookReadHistoryMapper bookReadHistoryMapper;
+     BooKReadHistoryRepository booKReadHistoryRepository;
+    SecurityService securityService;
+
+
+    // Thêm lịch sử đọc sách
+    public void addBookReadHistory(Account account, Book book) {
+        BookReadHistory history = BookReadHistory.builder()
+                .account(account)
+                .book(book)
+                .build();
+        booKReadHistoryRepository.save(history);
+    }
+
+    // Lấy danh sách lịch sử đọc sách theo tài khoản
+    public List<BookReadHistoryResponse> getBookReadHistoryByAccount() {
+        var account = securityService.getAccountByJWT();
+        return booKReadHistoryRepository.findByAccount_AccID(account.getAccID()).stream().map(bookReadHistoryMapper::toBookReadHistoryResponse).toList();
+    }
+
+    // Lấy lịch sử đọc sách theo tài khoản và sách
+    public List<BookReadHistoryResponse> getBookReadHistoryByAccountAndBook(int accID, int ID) {
+        return booKReadHistoryRepository.findByAccount_AccIDAndBook_ID(accID, ID).stream().map(bookReadHistoryMapper::toBookReadHistoryResponse).toList();
+    }
 }
