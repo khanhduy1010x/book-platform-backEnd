62bfa6f8a84a3ea7c86e6a90518a5f84e8db3863
thong <thongtchce181360@fpt.edu.vn>
2024-10-08 10:01:50 +0700

    addToCart

--- src/main/java/thebook/fshop/repository/BookRepository.java
+++ src/main/java/thebook/fshop/repository/BookRepository.java
@@ -3,6 +3,10 @@ package thebook.fshop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import thebook.fshop.entity.Book;
+
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    Optional<Book> findByID(int bookID);
 }
--- src/main/java/thebook/fshop/repository/CartRepository.java
+++ src/main/java/thebook/fshop/repository/CartRepository.java
@@ -3,8 +3,13 @@ package thebook.fshop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import thebook.fshop.entity.Cart;
+import thebook.fshop.entity.Account;
+import thebook.fshop.entity.Book;
+
+import java.util.List;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Integer> {
-
+    Cart findByAccount_AccIDAndBook_ID(int accID, int bookID);
+    List<Cart> findByAccount(Account account);
 }
