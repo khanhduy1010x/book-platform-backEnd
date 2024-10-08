f198e9d5fa58473990351411a3bb5f4432385d9b
Tran Tan Loi <LoiTTCE171879@fpt.edu.vn>
2024-10-08 13:15:52 +0700

    find book by id v1
    Fix conflics

--- src/main/java/thebook/fshop/controller/BookManagementController.java
+++ src/main/java/thebook/fshop/controller/BookManagementController.java
@@ -1,18 +1,21 @@
 package thebook.fshop.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import thebook.fshop.DTO.Request.BookDetailRequest;
 import thebook.fshop.DTO.Response.ApiResponse;
-import thebook.fshop.DTO.Response.BookResponse;
 import thebook.fshop.DTO.Response.ListBookByCateResponse;
+import thebook.fshop.entity.Book;
 import thebook.fshop.service.BookService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/book")
@@ -21,9 +24,15 @@ import java.util.List;
 @Slf4j
 public class BookManagementController {
     BookService bookService;
+
     @GetMapping("/list-book")
     public ApiResponse<List<ListBookByCateResponse>> viewListBook() {
         var response = bookService.getListBook();
         return ApiResponse.<List<ListBookByCateResponse>>builder().result(response).build();
+    }
+
+    @GetMapping("/book-detail")
+    public ApiResponse<Book> viewBookDetail(@RequestBody BookDetailRequest book) {
+        return ApiResponse.<Book>builder().result( bookService.getBookById(book)).build();
     }
 }
