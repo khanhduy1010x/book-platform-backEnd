79a282c0ed8deff42df80e05fdeacb51b4d2cf72
TVMPhung <115221527+corazonyy@users.noreply.github.com>
2024-10-08 14:09:46 +0700

    push code add-delete_to_favorite
    Fix conflics

--- src/main/java/thebook/fshop/exception/ErrorCode.java
+++ src/main/java/thebook/fshop/exception/ErrorCode.java
@@ -51,7 +51,10 @@ public enum ErrorCode {
     INVALID_OTP(1028, "Invalid OTP", HttpStatus.BAD_REQUEST),
     INVALID_AVATAR(1029, "Invalid OTP", HttpStatus.BAD_REQUEST),
     INVALID_FILE_NULL(1030, "Invalid OTP", HttpStatus.BAD_REQUEST),
-    PASSWORD_MISMATCH(1031,"Password error does not match user data" ,HttpStatus.BAD_REQUEST);
+    PASSWORD_MISMATCH(1031,"Password error does not match user data" ,HttpStatus.BAD_REQUEST),
+    DUPLICATE_BOOK(1029, "Already Exist", HttpStatus.BAD_REQUEST);
+
+
     private int code;
     private String message;
     HttpStatusCode statusCode;
---
+++ src/main/java/thebook/fshop/service/FavoriteService.java
@@ -0,0 +1,50 @@
+package thebook.fshop.service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import thebook.fshop.DTO.Request.AddToFavoriteRequest;
+import thebook.fshop.entity.WishList;
+import thebook.fshop.exception.AppException;
+import thebook.fshop.exception.ErrorCode;
+import thebook.fshop.repository.BookRepository;
+import thebook.fshop.repository.WishListRepository;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class FavoriteService {
+
+    WishListRepository wishListRepository;
+    SecurityService securityService;
+    BookRepository bookRepository;
+
+    // Add a favorite
+    public void addFavorite(AddToFavoriteRequest request) {
+        var account = securityService.getAccountByJWT();
+        var wishBook = wishListRepository.findByBook_IDAndAccount_AccID(request.getBookID(), account.getAccID());
+        log.info("Added to favorites: {}", wishBook);
+        if (wishBook != null) {
+            throw new AppException(ErrorCode.DUPLICATE_BOOK);
+        }
+        var wishList = WishList.builder()
+                .account(account)
+                .book(bookRepository.findById(request.getBookID()).orElseThrow(() -> new AppException(ErrorCode.NOT_FOUND)))
+                .build();
+        wishListRepository.save(wishList); // Save the wishlist item in the database
+    }
+
+    // Delete a favorite by ID
+    public void deleteFavorite(Integer id) {
+        var account = securityService.getAccountByJWT();
+        var wishBook = wishListRepository.findByBook_IDAndAccount_AccID(id, account.getAccID());
+        if (wishBook !=null) {
+            wishListRepository.deleteById(wishBook.getID()); // Delete the wishlist item by ID
+        } else {
+        throw new AppException(ErrorCode.NOT_FOUND);
+        }
+    }
+}
