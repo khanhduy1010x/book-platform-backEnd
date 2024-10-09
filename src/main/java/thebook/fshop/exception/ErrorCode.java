26bbe532cb12eda421886faf761a752360aef003
Tran Tan Loi <LoiTTCE171879@fpt.edu.vn>
2024-10-09 15:06:15 +0700

    thêm check lỗi cho ChangePassword v2

--- src/main/java/thebook/fshop/exception/ErrorCode.java
+++ src/main/java/thebook/fshop/exception/ErrorCode.java
@@ -51,7 +51,8 @@ public enum ErrorCode {
     INVALID_OTP(1028, "Invalid OTP", HttpStatus.BAD_REQUEST),
     INVALID_AVATAR(1029, "Invalid OTP", HttpStatus.BAD_REQUEST),
     INVALID_FILE_NULL(1030, "Invalid OTP", HttpStatus.BAD_REQUEST),
-    PASSWORD_MISMATCH(1031,"Password error does not match user data" ,HttpStatus.BAD_REQUEST);
+    PASSWORD_MISMATCH(1031,"Password error does not match user data" ,HttpStatus.BAD_REQUEST),
+    INVALID_NEW_PASSWORD(1032,"New Password and Confirmation New Password must be identical" ,HttpStatus.BAD_REQUEST);
     private int code;
     private String message;
     HttpStatusCode statusCode;
