43b43a52a79c53736e0dab2d309f64e16ae612bc
Tran Tan Loi <LoiTTCE171879@fpt.edu.vn>
2024-10-15 11:36:52 +0700

    update role for admin v1

---
+++ src/main/java/thebook/fshop/controller/MemberRoleUPController.java
@@ -0,0 +1,35 @@
+package thebook.fshop.controller;
+
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import thebook.fshop.DTO.Request.MemberRoleUpRequest;
+
+import thebook.fshop.DTO.Response.AccountResponse;
+import thebook.fshop.DTO.Response.ApiResponse;
+
+import thebook.fshop.entity.Account;
+import thebook.fshop.service.AccountService;
+
+@RestController
+@RequestMapping("/memberrole")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class MemberRoleUPController {
+    AccountService accountService;
+
+    @PostMapping("/up")
+    public ApiResponse<AccountResponse> upRole(@RequestBody @Valid MemberRoleUpRequest request) {
+        accountService.upRole(request);
+        return ApiResponse.<AccountResponse>builder().build();
+    }
+
+}
